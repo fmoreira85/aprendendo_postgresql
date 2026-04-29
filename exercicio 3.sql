ad3bc173a0f29fd3df58ef7ae6333161a3cd8998
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-04-29 13:43:06 -0400

    exer4

--- exercicio 3.sql
+++ exercicio 3.sql
@@ -1,1 +1,8 @@
--- Exercicio 3
+-- Liste clientes que fizeram mais de 1 pedido.
+SELECT
+    c.nome,
+    COUNT(p.id) AS total_pedidos
+FROM pedidos p
+JOIN clientes c ON p.cliente_id = c.id
+GROUP BY c.nome
+HAVING COUNT(p.id) > 1;
--- exercicio 4.sql
+++ exercicio 4.sql
@@ -1,1 +1,6 @@
--- Exercicio 4
+-- Liste todos os clientes, inclusive os que não fizeram pedidos.
+SELECT
+    c.nome,
+    p.valor
+FROM clientes c
+LEFT JOIN pedidos p ON c.id = p.cliente_id;
