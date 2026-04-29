f0ccd49daa4a4a98e3906c00070f1ab4347378f6
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-04-29 14:01:25 -0400

    exer9

--- exercicio 9.sql
+++ exercicio 9.sql
@@ -1,1 +1,9 @@
--- Exercicio 9
+-- Liste os 2 clientes que mais gastaram.
+SELECT
+    c.nome,
+    SUM(p.valor) AS total
+FROM pedidos p
+JOIN clientes c ON p.cliente_id = c.id
+GROUP BY c.nome
+ORDER BY total DESC
+LIMIT 2;
