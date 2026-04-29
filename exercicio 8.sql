c4649202f69205c85d5849d821652b1a36af8f1f
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-04-29 13:56:29 -0400

    exer8

--- exercicio 8.sql
+++ exercicio 8.sql
@@ -1,1 +1,7 @@
--- Exercicio 8
+-- Para cada cliente, mostre o maior pedido.
+SELECT
+    c.nome,
+    MAX(p.valor) AS maior_pedido
+FROM pedidos p
+JOIN clientes c ON p.cliente_id = c.id
+GROUP BY c.nome;
